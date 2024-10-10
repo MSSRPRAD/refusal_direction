71b142214cd780d6457a694ae36dc1e0eeccc15b
Malladi Pradyumna <f20210367@hyderabad.bits-pilani.ac.in>
2024-10-10 19:59:31 +0000

    Experiments with random directions and making first 10% of activations 0.

--- pipeline/utils/hook_utils.py
+++ pipeline/utils/hook_utils.py
@@ -38,6 +38,26 @@ def add_hooks(
         for h in handles:
             h.remove()
 
+def get_direction_ablation_input_pre_hook_first_ten(direction: Tensor):
+    def hook_fn(module, input):
+        nonlocal direction
+
+        if isinstance(input, tuple):
+            activation: Float[Tensor, "batch_size seq_len d_model"] = input[0]
+        else:
+            activation: Float[Tensor, "batch_size seq_len d_model"] = input
+
+        # direction = direction / (direction.norm(dim=-1, keepdim=True) + 1e-8)
+        # direction = direction.to(activation)
+        # activation -= (activation @ direction).unsqueeze(-1) * direction 
+        activation[:, :, :math.floor(activation.shape[2]*0.1)] = 0
+
+        if isinstance(input, tuple):
+            return (activation, *input[1:])
+        else:
+            return activation
+    return hook_fn
+
 def get_direction_ablation_input_pre_hook(direction: Tensor):
     def hook_fn(module, input):
         nonlocal direction
@@ -48,13 +68,36 @@ def get_direction_ablation_input_pre_hook(direction: Tensor):
             activation: Float[Tensor, "batch_size seq_len d_model"] = input
 
         direction = direction / (direction.norm(dim=-1, keepdim=True) + 1e-8)
-        direction = direction.to(activation) 
+        direction = direction.to(activation)
         activation -= (activation @ direction).unsqueeze(-1) * direction 
 
         if isinstance(input, tuple):
             return (activation, *input[1:])
         else:
             return activation
+    return hook_fn
+
+# Randomly make 10% of the activations zero
+import math
+def get_direction_ablation_output_hook_first_ten(direction: Tensor):
+    def hook_fn(module, input, output):
+        nonlocal direction
+
+        if isinstance(output, tuple):
+            activation: Float[Tensor, "batch_size seq_len d_model"] = output[0]
+        else:
+            activation: Float[Tensor, "batch_size seq_len d_model"] = output
+
+        # direction = direction / (direction.norm(dim=-1, keepdim=True) + 1e-8)
+        # direction = direction.to(activation)
+        # activation -= (activation @ direction).unsqueeze(-1) * direction 
+        activation[:, :, :math.floor(activation.shape[2]*0.1)] = 0
+
+        if isinstance(output, tuple):
+            return (activation, *output[1:])
+        else:
+            return activation
+
     return hook_fn
 
 def get_direction_ablation_output_hook(direction: Tensor):
@@ -85,6 +128,20 @@ def get_all_direction_ablation_hooks(
     fwd_hooks = [(model_base.model_attn_modules[layer], get_direction_ablation_output_hook(direction=direction)) for layer in range(model_base.model.config.num_hidden_layers)]
     fwd_hooks += [(model_base.model_mlp_modules[layer], get_direction_ablation_output_hook(direction=direction)) for layer in range(model_base.model.config.num_hidden_layers)]
 
+    print(fwd_pre_hooks)
+    print(fwd_hooks)
+    return fwd_pre_hooks, fwd_hooks
+
+def get_all_direction_ablation_hooks_first_ten(
+    model_base,
+    direction: Float[Tensor, 'd_model'],
+):
+    fwd_pre_hooks = [(model_base.model_block_modules[layer], get_direction_ablation_input_pre_hook_first_ten(direction=direction)) for layer in range(model_base.model.config.num_hidden_layers)]
+    fwd_hooks = [(model_base.model_attn_modules[layer], get_direction_ablation_output_hook_first_ten(direction=direction)) for layer in range(model_base.model.config.num_hidden_layers)]
+    fwd_hooks += [(model_base.model_mlp_modules[layer], get_direction_ablation_output_hook_first_ten(direction=direction)) for layer in range(model_base.model.config.num_hidden_layers)]
+
+    print(fwd_pre_hooks)
+    print(fwd_hooks)
     return fwd_pre_hooks, fwd_hooks
 
 def get_directional_patching_input_pre_hook(direction: Float[Tensor, "d_model"], coeff: Float[Tensor, ""]):
@@ -100,6 +157,27 @@ def get_directional_patching_input_pre_hook(direction: Float[Tensor, "d_model"],
         direction = direction.to(activation) 
         activation -= (activation @ direction).unsqueeze(-1) * direction 
         activation += coeff * direction
+
+        if isinstance(input, tuple):
+            return (activation, *input[1:])
+        else:
+            return activation
+    return hook_fn
+
+def get_activation_addition_input_pre_hook_first_ten(vector: Float[Tensor, "d_model"], coeff: Float[Tensor, ""]):
+    def hook_fn(module, input):
+        nonlocal vector
+
+        if isinstance(input, tuple):
+            activation: Float[Tensor, "batch_size seq_len d_model"] = input[0]
+        else:
+            activation: Float[Tensor, "batch_size seq_len d_model"] = input
+
+        # vector = vector.to(activation)
+        # activation += coeff * vector
+
+
+        activation[:, :, :math.floor(activation.shape[2]*0.1)] = 0
 
         if isinstance(input, tuple):
             return (activation, *input[1:])
